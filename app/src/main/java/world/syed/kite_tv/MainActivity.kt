59f9b9fc21897aae3b22c4c0101556419f672c49
Syed Ahkam <smahkam57@gmail.com>
2024-06-15 22:25:10 +0530

    feat: get a basic webview going

--- app/src/main/java/world/syed/kite_tv/MainActivity.kt
+++ app/src/main/java/world/syed/kite_tv/MainActivity.kt
@@ -2,10 +2,21 @@ package world.syed.kite_tv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webview: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webview = findViewById(R.id.webview)
+
+        var webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webview.loadUrl("https://syed.world");
     }
 }
