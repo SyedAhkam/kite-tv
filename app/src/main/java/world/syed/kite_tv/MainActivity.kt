9d9583ce55521d3548b6319680d494b708ccfd5c
Syed Ahkam <smahkam57@gmail.com>
2024-06-18 00:50:00 +0530

    feat: get it done

--- app/src/main/java/world/syed/kite_tv/MainActivity.kt
+++ app/src/main/java/world/syed/kite_tv/MainActivity.kt
@@ -4,6 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebChromeClient
+import android.util.Log
+
+class CustomWebViewClient : WebViewClient() {
+    override fun shouldOverrideUrlLoading(
+        view: WebView,
+        request: WebResourceRequest
+    ): Boolean {
+        view.loadUrl(request.url.toString())
+        return false
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webview: WebView
@@ -13,10 +27,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         webview = findViewById(R.id.webview)
+        webview.setWebViewClient(CustomWebViewClient())
+        webview.setWebChromeClient(WebChromeClient());
+
 
         var webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        webview.loadUrl("https://syed.world");
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setSupportMultipleWindows(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setUserAgentString("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36");
+
+        webview.setClickable(true);
+        webview.setInitialScale(1);
+
+        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webview.setScrollbarFadingEnabled(false);
+
+        webview.loadUrl("https://kite.zerodha.com");
+
+        val cursorLayout: CursorLayout = findViewById(R.id.cursor_layout)
+    }
+
+    override fun onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
